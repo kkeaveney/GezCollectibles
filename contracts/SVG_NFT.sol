cee3cc8f292389fa527abcc660f613934aaeb4ae
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-10-13 18:30:44 +0100

    SVG init

---
+++ contracts/SVG_NFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <=0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import "hardhat/console.sol";
+
+contract SVG {
+
+}
