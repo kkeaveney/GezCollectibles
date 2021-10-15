f73888f756b9646e7ccf492977a08fb779a49178
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-10-15 11:25:18 +0100

    svgToImageURI

--- contracts/SVG_NFT.sol
+++ contracts/SVG_NFT.sol
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <=0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIstorage.sol";
+import "base64-sol/base64.sol";
 
-contract SVG {
 
+contract SVGNFT is ERC721URIStorage {
+    uint256 tokenCOunter;
+    constructor() ERC721("SVG NFT", "svgNFT") {
+        tokenCounter = 0;
+    }
+
+    function create(string memory svg) public  {
+        _safeMint(msg.sender, tokenCounter);
+        string memory imageURI = svgToImageURI(svg);
+        tokenCounter = tokenCOunter + 1;
+    }
+
+    function svgToImageURI() public pure returns (string memory){
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        string imageURL = string(abi.encodePacked(baseURL, svgBase64Encoded));
+        return imageURI;
+    }
 }
