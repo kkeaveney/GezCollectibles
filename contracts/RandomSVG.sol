ed89c8f83c5c9076ff4d9401d6cf82683fdc8520
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-10-20 16:44:30 +0100

    VRFConsumerBase params

--- contracts/RandomSVG.sol
+++ contracts/RandomSVG.sol
@@ -2,10 +2,15 @@
 pragma solidity >=0.6.0 <=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract RandomSVG is ERC721URIStorage {
 
     constructor() ERC721("RandomSVG", "rsvg") {
-        
+
+    }
+
+    function create() public {
+
     }
 }
