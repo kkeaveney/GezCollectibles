ca07f1f5121c7b7cfe51855a88662dd1e8be632b
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-11-12 09:05:53 +0000

    new contract address

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,7 @@ require("./tasks/mint")
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "rinkeby",
   // paths: {
   //   artifacts: './src/artifacts',
   // },
