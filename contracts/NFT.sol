17426eabd9e40f88bec36cd82d02793f31a36ecc
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-11-16 16:23:02 +0000

    refactor mint eith tokenURI

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -9,6 +9,7 @@ import "hardhat/console.sol";
   contract NFT is ERC721Enumerable, Ownable {
 
     using SafeMath for uint256;
+    using Strings for uint256;
 
     struct NFTDetail {
       uint256 first_encounter;
@@ -47,6 +48,14 @@ import "hardhat/console.sol";
     string private baseURI;
 
     mapping (uint => bool) public sold;
+
+    // optional mapping for token URIs
+    mapping (uint => string) private _tokenURIs;
+
+    mapping (uint => uint) public price;
+
+    // Base URI
+    string private _baseURIextended;
 
 
     /**
@@ -94,7 +103,7 @@ import "hardhat/console.sol";
     /**
     Mint tokens
      */
-    function mint(uint256 numOfTokens) public payable {
+    function mint(string memory tokenURI, uint256 numOfTokens, uint256 _price) public payable {
       require(saleIsActive, "Sale must be active for minting");
       require(numOfTokens <= MAX_PURCHASE, 'Can only mint 10 NFTs at a time');
       require(totalSupply().add(numOfTokens) <= MAX_NFTS, "At max Supply");
@@ -103,11 +112,23 @@ import "hardhat/console.sol";
         for(uint i=1; i<=numOfTokens; i++) {
           uint256 _tokenId = totalSupply().add(1);
           _safeMint(msg.sender, _tokenId);
+          price[_tokenId] = _price;
+          //_setTokenURI(_tokenId, _tokenURI);
           uint256 first_encounter = block.timestamp;
           _nftDetail[_tokenId] = NFTDetail(first_encounter);
           emit TokenMinted(_tokenId, msg.sender, first_encounter);
         }
     }
+
+  //   function mint(string memory _tokenURI, uint _price) public onlyOwner returns (bool) {
+  //   uint _tokenId = tokenCounter + 1;
+  //   price[_tokenId] = _price;
+
+  //   _mint(address(this), _tokenId);
+  //   _setTokenURI(_tokenId, _tokenURI);
+  //   tokenCounter ++;
+  //   return true;
+  // }
 
     /**
     select Gold NFT
@@ -198,6 +219,29 @@ import "hardhat/console.sol";
       _transfer(address(this), msg.sender, id); //nft to user
       _owner.transfer(msg.value); //eth to owner
       sold[id] = true; //nft is sold
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+      string memory _tokenURI = _tokenURIs[tokenId];
+      string memory base = _baseURI();
+
+      // If there is no base URI, return the token URI.
+      if (bytes(base).length == 0) {
+                return _tokenURI;
+      }
+      // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+      if (bytes(_tokenURI).length > 0) {
+          return string(abi.encodePacked(base, _tokenURI));
+      }
+      // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+      return string(abi.encodePacked(base, tokenId.toString()));
+  }
+
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+      require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+      _tokenURIs[tokenId] = _tokenURI;
     }
 
 }
